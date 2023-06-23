ee1eeeadd591595e05c358fb4b5017770ff39101
João Paulo Kuwano <joaopaulokuwano@gmail.com>
2023-06-22 23:52:06 -0300

    feat: index page

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Button, Flex } from '@chakra-ui/react'
+import { Header } from '../../components/header'
+
+export const IndexPage = () => {
+  return (
+    <Box>
+      <Header />
+    </Box>
+  )
+}
